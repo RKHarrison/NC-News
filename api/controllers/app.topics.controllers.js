3540041cde42531f78074c0a502a87208a3139bb
Richard Harrison <r.harr.k@gmail.com>
2024-05-28 11:42:10 +0100

    requires model into controller and completes happy-path functionality for GET /api/topics endpoint

--- api/controllers/app.topics.controllers.js
+++ api/controllers/app.topics.controllers.js
@@ -1,5 +1,7 @@
+const {fetchTopics} = require('../models/app.topics.models')
+
 exports.getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
-    res.status.send({ topics });
-  });
+    res.status(200).send({ topics });
+  })
 };
