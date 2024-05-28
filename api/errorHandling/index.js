e16d542357aee2c643220c7362bc96120642e73b
Richard Harrison <r.harr.k@gmail.com>
2024-05-28 15:14:17 +0100

    catches previously unspecified errors for the sake of debugging

--- api/errorHandling/index.js
+++ api/errorHandling/index.js
@@ -2,6 +2,12 @@ exports.handleGeneric404Errors = ('*', (req, res) => {
   res.status(404).send({msg: "Route Not Found"})
 })
 
+exports.handlePsqlErrors = (err, req,res,next) => {
+  console.log(err);
+  if (err.code) console.log(err);
+  if (err.msg)  console.log(err);
+}
+
 exports.handleServerErrors = (err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
 };
