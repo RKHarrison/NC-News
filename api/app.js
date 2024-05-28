6e15ebfa5720ef83ce924ba8af78e47b21abff52
Richard Harrison <r.harr.k@gmail.com>
2024-05-28 15:12:07 +0100

    changes exampleResponse to reflect change in created_at format

--- api/app.js
+++ api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {getTopics, getEndpoints} = require('./controllers/app.topics.controllers')
+const {getTopics} = require('./controllers/app.api.topics.controllers')
+const {getEndpoints} = require('./controllers/app.api.controllers')
 const {handleGeneric404Errors, handleServerErrors} = require('./errorHandling/index')
 const app = express()
 
 app.get("/api/topics", getTopics)
 app.get("/api", getEndpoints)
-
+// app.get("/api/articles/:article_id", getArticlesById)
 
 app.use(handleGeneric404Errors)
 app.use(handleServerErrors)
