b4e5bf13ff8e75795ea58c7375acdaa7d65584ea
Richard Harrison <r.harr.k@gmail.com>
2024-05-28 11:50:02 +0100

    includes .catch(next) on getTopics controller

--- api/controllers/app.topics.controllers.js
+++ api/controllers/app.topics.controllers.js
@@ -3,5 +3,5 @@ const {fetchTopics} = require('../models/app.topics.models')
 exports.getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
     res.status(200).send({ topics });
-  })
+  }).catch(next)
 };
