34fad7673138b369b31a8e3abcaf23883179339f
Richard Harrison <r.harr.k@gmail.com>
2024-05-29 22:03:16 +0100

    refactors sqlQuery in fetchArticles model for clarity

--- api/models/app.api.articles.models.js
+++ api/models/app.api.articles.models.js
@@ -2,23 +2,18 @@ const db = require("../../db/connection");
 
 exports.fetchArticles = () =>{
     const sqlQuery = `
-    SELECT     a.created_at, 
-    a.article_id, 
-    a.author, 
-    title, 
-    topic, 
-    a.votes, 
-    a.article_id, 
-    COUNT(comment_id) AS comment_count
+SELECT  a.created_at, 
+        a.title,
+        a.created_at, 
+        a.article_id, 
+        a.author, 
+        a.title, 
+        a.topic, 
+        a.votes,
+        COUNT(comment_id) AS comment_count
 FROM articles a
 LEFT JOIN comments c ON a.article_id = c.article_id
-GROUP BY   a.title,
-    a.created_at, 
-    a.article_id, 
-    a.author, 
-    a.title, 
-    a.topic, 
-    a.votes
+GROUP BY a.title, a.created_at, a.article_id, a.author, a.title, a.topic, a.votes
 ORDER BY a.created_at DESC;
 `
 
