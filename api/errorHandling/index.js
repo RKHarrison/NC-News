8c61ad08d40e70036c8a71fceb329b68f38f99cb
Richard Harrison <r.harr.k@gmail.com>
2024-05-29 20:22:12 +0100

    err handling housekeeping after testing api util checkExists

--- api/errorHandling/index.js
+++ api/errorHandling/index.js
@@ -15,7 +15,6 @@ exports.handlePsqlErrors = (err, req,res,next) => {
   }
   if (err.code === '22P02') {
     res.status(400).send({msg: `Bad ${req.method} Request`})
-    // console.log(err.code);
   } else next(err)
 }
 
