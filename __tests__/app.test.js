1d4b0fbd2141e7b0dc027e90b73361f13c41a032
Richard Harrison <r.harr.k@gmail.com>
2024-05-29 09:55:18 +0100

    updates 200 test paramaters to be explicit rather than reference example case in endpoints.json

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -58,8 +58,15 @@ describe('GET /api/articles/:article_id', () => {
         .get('/api/articles/1')
         .expect(200)
         .then(({body:article}) => {
-            const expectedResult = endpointsJsonFile["GET /api/articles/:article_id"].exampleResponse.article
-            expect(article).toMatchObject(expectedResult)
+            expect(article).toMatchObject({
+              article_id : 1,
+              author : "butter_bridge",
+              title : "Living in the shadow of a great man",
+              body : "I find this existence challenging",
+              created_at : "2020-07-09T20:11:00.000Z",
+              votes : 100,
+              article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+            })
         })
     });
     it("404: responds with 'Not Found' when given valid but non-existing id", () => {
