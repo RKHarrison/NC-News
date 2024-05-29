7781b9944b8f3cd958157b92d4a8959572a3d7d8
Richard Harrison <r.harr.k@gmail.com>
2024-05-29 14:51:51 +0100

    does some housekeeping after completing endpoint

--- api/errorHandling/index.js
+++ api/errorHandling/index.js
@@ -12,10 +12,10 @@ exports.handlePsqlErrors = (err, req,res,next) => {
   if (err.code === '23502' || err.code === '23503') {
     res.status(400).send({msg: 'Bad Post Request'})
   }
-  console.log(err.code);
   if (err.code === '22P02') {
     res.status(400).send({msg: 'Bad Request'})
-} else next(err)
+    // console.log(err.code);
+  } else next(err)
 }
 
 
