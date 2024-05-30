a84e3cb122abd895c5861f5bd3a552df8b16e3b6
Richard Harrison <r.harr.k@gmail.com>
2024-05-30 21:25:33 +0100

    adds sad path testing for order and sort

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -405,7 +405,7 @@ describe("GET api/articles?filter_by=:filterTerm", () => {
 });
 
 describe("GET /api/articles?order=ASCE/DESCS&sort_by=any_column", () => {
-  it('"200: sorts articles by default column, ascending', () => {
+  it("200: sorts articles by default column, ascending", () => {
     return request(app)
       .get("/api/articles?order=ASC")
       .expect(200)
@@ -414,7 +414,7 @@ describe("GET /api/articles?order=ASCE/DESCS&sort_by=any_column", () => {
         expect(articles).toBeSortedBy("created_at", { ascending: true });
       });
   });
-  it('"200: sorts articles by any valid column, default descending', () => {
+  it("200: sorts articles by any valid column, default descending", () => {
     return request(app)
       .get("/api/articles?sort_by=article_id")
       .expect(200)
@@ -423,7 +423,7 @@ describe("GET /api/articles?order=ASCE/DESCS&sort_by=any_column", () => {
         expect(articles).toBeSortedBy("article_id", { descending: true });
       });
   });
-  it('"200: sorts articles by a different valid column, ascending', () => {
+  it("200: sorts articles by a different valid column, ascending", () => {
     return request(app)
       .get("/api/articles?sort_by=title&order=ASC")
       .expect(200)
@@ -431,5 +431,29 @@ describe("GET /api/articles?order=ASCE/DESCS&sort_by=any_column", () => {
         expect(articles).toHaveLength(13);
         expect(articles).toBeSortedBy("title", { ascending: true });
       });
-  }); 
+  });
+  it("400: responds 'Bad Order Request' when given an unaccepted order term", () => {
+    return request(app)
+      .get("/api/articles?order=NOT_ALLOWED")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Bad Order Request');
+      });
+  });
+  it("400: responds 'Bad Sort Request' when given an unaccepted order term", () => {
+    return request(app)
+      .get("/api/articles?sort_by=NOT_ALLOWED")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Bad Sort Request');
+      });
+  });
+  it("400: responds 'Bad Query Request' when both query terms are unaccepted", () => {
+    return request(app)
+      .get("/api/articles?order=NOT_ALLOWED&sort_by=NOT_ALLOWED")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Bad Query Request');
+      });
+  });
 });
