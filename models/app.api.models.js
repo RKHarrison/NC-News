ba81a964d4e214605c086f524dae38292e138462
Richard Harrison <r.harr.k@gmail.com>
2024-05-30 18:04:17 +0100

    changed path for /api fs readfile after changing file structure for hosting

--- models/app.api.models.js
+++ models/app.api.models.js
@@ -1,7 +1,7 @@
 const fs = require("fs/promises");
 
 exports.fetchEndpoints = () => {
-    const pathToEndpointsJson = `${__dirname}/../../endpoints.json`
+    const pathToEndpointsJson = `${__dirname}/../endpoints.json`
   
     return fs.readFile(pathToEndpointsJson, "utf-8").then((data) => {
       const parsedData = JSON.parse(data)
